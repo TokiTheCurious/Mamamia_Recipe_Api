8f4003c5bc3af0e8cbb49ef649d21d42f2f50576
toki monkey <tokithemonkey@gmail.com>
2020-03-07 03:05:32 +0900

    crud operations

--- functions/controllers/recipe-ctrl.js
+++ functions/controllers/recipe-ctrl.js
@@ -33,11 +33,119 @@ createRecipe = (req, res) => {
         })
 }
 
-helloRecipe = (req, res) => {
-    res.send("Hello World!")
+updateRecipe = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'Need a body to update'
+        })
+    }
+
+    Recipe.findOne({ _id: req.params.id }, (err, recipe) => {
+        if (err){
+            return res.status(404).json({
+                err,
+                message: "Recipie not found fool"
+            })
+        }
+        recipe.title = body.title
+        recipe.description = body.description
+        recipe.source = body.source
+        recipe.originalUrl = body.originalUrl
+        recipe.yield = body.yield
+        recipe.active = body.active
+        recipe.image = body.image
+        recipe.ingredients = body.ingredients
+        recipe.instructions = body.instructions
+        recipe.notes = body.notes
+
+        recipe
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: recipe._id,
+                    message: "Recipe updated!"
+                })
+            })
+            .catch(error => {
+                return res.status(400).json({
+                    error,
+                    message: 'Recipe not updated'
+                })
+            })
+    })
+}
+deleteRecipe = async (req, res) => {
+    await Recipe.findOneAndDelete({ _id: req.params.id }, (err, recipe) => {
+        if (err) {
+            return res.status(400).json({success: false, error: err})
+        }
+
+        if (!recipe) {
+            return res
+                .status(404)
+                .json({success: false, error: `Recipe not found`})
+        }
+
+        return res.status(200).json({success: true, data: recipe})
+    }).catch(error => console.log(error))
+}
+
+searchRecipeByTitle = async (req, res) => {
+    console.log("hit search")
+    await Recipe.find({ title: {$regex: req.query.query, $options: 'i'}}, (err, recipe) => {
+        if (err){
+            return res.status(400).json({success:false, error: err})
+        }
+
+        if (!recipe.length) {
+            return res
+                .status(404)
+                .json({success:false, error: `Recipe not found`})
+        }
+        return res.status(200).json({success:true, data: recipe})
+    })
+}
+
+getRecipeById = async (req, res) => {
+    console.log("hit id endpoint")
+    await Recipe.findOne({ _id: req.params.id }, (err, recipe) => {
+        if (err){
+            return res.status(400).json({success:false, error: err})
+        }
+
+        if (!recipe) {
+            return res
+                .status(404)
+                .json({success:false, error: `Recipe not found`})
+        }
+        return res.status(200).json({success:true, data: recipe})
+    })
+}
+
+getRecipies = async (req, res) => {
+    console.log("hit get all")
+    await Recipe.find({}, (err, recipies) => {
+        if(err){
+            return res.status(400).json({success: false, error: err})
+        }
+        if (!recipies.length){
+            return res
+                .status(404)
+                .json({ success: false, error: `Recipe not found`})
+        }
+        return res.status(200).json({ success: true, data: recipies })
+    }).catch(err => console.log(err))
 }
 
 module.exports = {
     createRecipe,
-    helloRecipe
+    updateRecipe,
+    deleteRecipe,
+    getRecipeById,
+    searchRecipeByTitle,
+    getRecipies
 }
--- functions/routes/recipe-router.js
+++ functions/routes/recipe-router.js
@@ -5,6 +5,10 @@ const RecipeCtrl = require('../controllers/recipe-ctrl')
 const router = express.Router()
 
 router.post('/recipe', RecipeCtrl.createRecipe)
-router.get('/recipe', RecipeCtrl.helloRecipe)
+router.put('/recipe/:id', RecipeCtrl.updateRecipe)
+router.get('/recipe', RecipeCtrl.getRecipies)
+router.get('/recipe/:id', RecipeCtrl.getRecipeById)
+router.get('/search?:query', RecipeCtrl.searchRecipeByTitle)
+router.delete('/recipe', RecipeCtrl.deleteRecipe)
 
 module.exports = router
